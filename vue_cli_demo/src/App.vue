7da7ee5d50d611722ada3d763e8da8acbb09f91c
ListenerSun <ListenerSun@163.com>
2020-01-13 00:55:04 +0800

    add 使用vue-cli 创建项目

---
+++ vue_cli_demo/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+<!--    使用映射组件-->
+    <HelloWorld/>
+  </div>
+</template>
+
+<script>
+// 引入组件
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'App',
+  /* 映射组件 */
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+</style>
