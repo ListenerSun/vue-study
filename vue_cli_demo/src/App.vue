658fafd416d937bf8ffff775a572c004300ecb48
ListenerSun <ListenerSun@163.com>
2020-01-19 00:16:12 +0800

    add vue数据添加交互

--- vue_cli_demo/src/App.vue
+++ vue_cli_demo/src/App.vue
@@ -1,20 +1,55 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-<!--    使用映射组件-->
-    <HelloWorld/>
+  <div>
+    <header class="site-header jumbotron">
+      <div class="container">
+        <div class="row">
+          <div class="col-xs-12">
+            <h1>请发表对Vue的评论</h1>
+          </div>
+        </div>
+      </div>
+    </header>
+    <div class="container">
+      <Add :addComment="addComment"></Add>
+      <List :comments="comments"></List>
+    </div>
   </div>
 </template>
 
 <script>
 // 引入组件
-import HelloWorld from './components/HelloWorld'
+import Add from './components/Add'
+import List from './components/List'
 
 export default {
-  name: 'App',
   /* 映射组件 */
   components: {
-    HelloWorld
+    Add,
+    List
+  },
+  data (){
+    return {
+      comments:[ //数据在哪个组件中,更新数据的行为(方法)就应该定义在哪个组件中
+        {
+          name: 'Listener',
+          content: 'I Am ListenerSun'
+        },
+        {
+          name: 'WBY',
+          content: 'I Am WBY'
+        },
+        {
+          name: 'XTP',
+          content: 'I am XTP'
+        }
+      ]
+    }
+  },
+  methods:{
+    //添加评论
+    addComment(comment){
+      this.comments.unshift(comment)
+    }
   }
 }
 </script>
---
+++ vue_cli_demo/src/components/List.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="col-md-8">
+    <h3 class="reply">评论回复：</h3>
+    <h2 style='display: none'>暂无评论，点击左侧添加评论！！！</h2>
+    <ul class="list-group">
+
+      <Item v-for="(comment,index) in comments" :key="index" :comment="comment"></Item>
+    </ul>
+  </div>
+</template>
+
+<script>
+  import Item from './Item'
+  export default {
+    // 声明接收属性, 接收父组件传来的属性:这个属性就会成为组件对象的属性
+    // 当前的 this 对象为组件对象，根VM用法一样
+    props: ['comments'], //只制定了属性名
+    components:{
+      Item
+    }
+  }
+</script>
+
+<style scoped>
+  .reply {
+    margin-top: 0px;
+  }
+
+  li {
+    transition: .5s;
+    overflow: hidden;
+  }
+
+  .handle {
+    width: 40px;
+    border: 1px solid #ccc;
+    background: #fff;
+    position: absolute;
+    right: 10px;
+    top: 1px;
+    text-align: center;
+  }
+
+  .handle a {
+    display: block;
+    text-decoration: none;
+  }
+
+  .list-group-item .centence {
+    padding: 0px 50px;
+  }
+
+  .user {
+    font-size: 22px;
+  }
+</style>
